2fb10c838b0fc95a19eaa50b6c667e161147fd8b
Joshua Reese <reesejosh@hotmail.com>
2020-06-19 14:28:23 -0400

    first upload of the app

---
+++ Bank.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Bank
+{
+	ArrayList<Customer> customers = new ArrayList<Customer>();
+	
+	public void addCustomer(Customer customer)
+	{
+		customers.add(customer);
+	}
+	public Customer getCustomer(int account)
+	{
+		return customers.get(account);
+	}
+	ArrayList<Customer> getCustomers()
+	{
+		return customers;
+	}
+}
---
+++ InvalidAccountTypeException.java
@@ -0,0 +1,8 @@
+
+public class InvalidAccountTypeException extends Exception
+{
+	public InvalidAccountTypeException()
+	{
+		super ("Invalid Account Type Selected");
+	}
+}
